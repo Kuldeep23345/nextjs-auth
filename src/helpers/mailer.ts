01be7dc8027a2dcda397648e898c28488bec4458
Kuldeep <kunalrajput67718@gmail.com>
2025-09-22 16:45:33 +0530

    nodemailer config done

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async ({ email, emailType, userId }:any) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    const mailOptions = {
+      from: "kunal@kunal.ai",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "veriry you email" : "Reset your password",
+      html: "<b>Hello world?</b>", // HTML body
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions)
+
+    return mailResponse
+  } catch (error:any) {
+    throw new Error(error.message)
+  }
+};
