64dafc91515dc8acff186d5340ca12e1c1f306a0
Kuldeep <kunalrajput67718@gmail.com>
2025-09-23 15:39:49 +0530

    user registerd done

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,28 +1,39 @@
 import { connectDB } from "@/db/db";
-import User from "@/models/userModel"
-import { NextRequest,NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
-connectDB()
+connectDB();
 
-export async function POST(request:NextRequest) {
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = request.json();
+    const { username, email, password }: any = reqBody;
+    console.log(reqBody);
 
-    try {
-     const reqBody=   request.json()
-     const {username,email,password} = reqBody
-     console.log(reqBody);
-
-     const existedUser = await User.findOne({email})
-     if(existedUser){
-        return NextResponse.json({error:"User already exists"},{status:400})
-
-     }
-     const hashedPassword = await  bcrypt.hash(password,10)
-
-     if(!hashedPassword)
-        
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},{status:500})
-        
+    const existedUser = await User.findOne({ email });
+    if (existedUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await sendEmail({ email, emailType: "VERIFY", userId: newUser._id });
+
+    return NextResponse.json(
+      { message: "User registerd successfully", success: true, newUser },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
